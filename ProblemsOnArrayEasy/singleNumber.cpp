55cb29e77e8cfdda94dc3380cbf4a36a8789aea5
Sujjal1 <rocksujl02@gmail.com>
2026-02-09 11:41:10 -0600

    Find single number form array

---
+++ ProblemsOnArrayEasy/singleNumber.cpp
@@ -0,0 +1,27 @@
+// Finding the number that is single in the array
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int xorr = 0;
+        for(int i = 0; i < nums.size(); i++){
+            xorr = xorr ^ nums[i];
+        }   
+        return xorr;
+    }
+};
+
+int main()
+{
+    vector<int> a = {1, 3, 1, 0, 0, 2, 3, 4, 4};
+    Solution Obj;
+    int num = Obj.singleNumber(a);
+    cout << num << endl;
+}
