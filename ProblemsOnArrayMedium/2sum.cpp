4d9f9784e357243ee1081241d1f316d40136b127
Sujjal1 <rocksujl02@gmail.com>
2026-02-13 13:35:18 -0600

    returning the index of the numbers in the array whose sum is target

---
+++ ProblemsOnArrayMedium/2sum.cpp
@@ -0,0 +1,31 @@
+//fidning two numbers whoae sum is target from the array
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> data;
+        for(int i = 0; i < nums.size(); i++){
+            int val = target - nums[i];
+            if(data.find(val)!=data.end()){
+                return {data[val], i};
+            }
+            else{
+                data[nums[i]] = i;
+            }
+        }
+        return {-1, -1};
+    }
+};
+
+int main(){
+    vector<int> a = {1,2, 1, 1, 2, 3, 0};
+    Solution Obj;
+    vector<int> b = Obj.twoSum(a, 4);
+    cout<<b[0]<<" "<< b[1]<< endl;
+}
+
